c0d4796f5a95e782fc2a16f42dea960a0baa8597
Hiram K <hiram.kamau@outlook.com>
2021-08-16 17:26:18 +0300

    Move login routine to separate class

--- pee-records-controller/src/main/java/com/github/idelstak/pee/records/controller/App.java
+++ pee-records-controller/src/main/java/com/github/idelstak/pee/records/controller/App.java
@@ -3,26 +3,14 @@
  */
 package com.github.idelstak.pee.records.controller;
 
-import com.github.idelstak.pee.records.controller.database.DatabaseConnectionController;
-import com.github.idelstak.pee.records.controller.login.LoginController;
-import com.github.idelstak.pee.records.database.DatabaseAccess;
-import com.github.idelstak.pee.records.view.api.FxmlParent;
-import com.github.idelstak.pee.records.view.database.DatabaseConnectionFxml;
-import com.github.idelstak.pee.records.view.login.LoginFxml;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.prefs.Preferences;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javax.sql.DataSource;
 
 /**
  *
@@ -34,50 +22,17 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Preferences prefs = Preferences.userNodeForPackage(App.class);
+        Scene scene = new Scene(new BorderPane(), Color.WHITE);
+        //Check whether database connection can be established
+        LoginAttempt attempt = new LoginAttempt();
 
-        String user = prefs.get("user.name", "root");
-        char[] password = prefs.get("password", "corner-dicing").toCharArray();
-        String url = prefs.get("jdbc.url", "jdbc:mysql://localhost/PeeCalendar");
+        Optional<String> user = attempt.getUser();
+        
+        LOGGER.log(Level.INFO, "Logged in user: {0}", user);
 
-        DatabaseAccess databaseAccess = new DatabaseAccess(user, password, url);
-
-        try {
-            DataSource dataSource = databaseAccess.getDataSource();
-            Parent root;
-
-            try (Connection conn = dataSource.getConnection()) {
-                LoginFxml loginFxml = new LoginFxml();
-                LoginController loginController = new LoginController(dataSource);
-
-                FxmlParent fxmlParent = new FxmlParent(loginFxml, loginController);
-                root = fxmlParent.get();
-            } catch (SQLException ex) {
-                DatabaseConnectionFxml dbConnFxml = new DatabaseConnectionFxml();
-                DatabaseConnectionController dbConnController = new DatabaseConnectionController();
-
-                FxmlParent fxmlParent = new FxmlParent(dbConnFxml, dbConnController);
-                root = fxmlParent.get();
-            }
-
-            Scene scene = new Scene(root);
-
-            primaryStage.setTitle("Pee Records");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception ex) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-
-            alert.setTitle("Database Connection Error");
-            alert.setHeaderText("Failed to connect");
-            alert.setContentText(ex.getMessage().substring(ex.getMessage().indexOf(":") + 2));
-
-            Optional<ButtonType> btn = alert.showAndWait();
-
-            btn.ifPresent(btnType -> Platform.exit());
-
-            alert.setOnHidden(eh -> Platform.exit());
-        }
+        primaryStage.setTitle("Pee Records");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**

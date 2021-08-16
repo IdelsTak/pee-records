a6a7c9429feed4b31991c507232d65f501c3aad7
Hiram K <hiram.kamau@outlook.com>
2021-08-16 22:03:04 +0300

    Fix java.nio.file.FileSystemNotFoundException when access is in a jar file

--- pee-records-dao/src/main/java/com/github/idelstak/pee/records/dao/api/InitializeTables.java
+++ pee-records-dao/src/main/java/com/github/idelstak/pee/records/dao/api/InitializeTables.java
@@ -4,13 +4,17 @@
 package com.github.idelstak.pee.records.dao.api;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.spi.FileSystemProvider;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collections;
 import javax.sql.DataSource;
 import org.apache.ibatis.jdbc.ScriptRunner;
 
@@ -38,7 +42,29 @@ public class InitializeTables {
 
         try {
             URL url = getClass().getResource("/scripts/peecalendar-tables.sql");
-            path = Paths.get(url.toURI());
+            URI uri = url.toURI();
+
+            //Important: tell the executable that the file is in a jar
+            //Remember, when running the compiled source code
+            //the file is in a normal filesystem folder
+            //but, when the code is packed into a jar
+            //the path should check inside a jar and not inside
+            //a filesystem folder
+            if ("jar".equals(uri.getScheme())) {
+                for (FileSystemProvider fsp : FileSystemProvider.installedProviders()) {
+                    if (fsp.getScheme().equalsIgnoreCase("jar")) {
+                        try {
+                            fsp.getFileSystem(uri);
+                        } catch (FileSystemNotFoundException exception) {
+                            //Filesystem was not found
+                            //so, initialize it first
+                            fsp.newFileSystem(uri, Collections.emptyMap());
+                        }
+                    }
+                }
+            }
+            
+            path = Paths.get(uri);
         } catch (URISyntaxException ex) {
             throw new IOException(ex);
         }

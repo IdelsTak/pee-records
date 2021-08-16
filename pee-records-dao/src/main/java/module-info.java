2556c2aed47256e1f7f9509d2066a1cb78bbace5
Hiram K <hiram.kamau@outlook.com>
2021-08-16 21:33:42 +0300

    Update dependencies

--- pee-records-dao/src/main/java/module-info.java
+++ pee-records-dao/src/main/java/module-info.java
@@ -4,8 +4,10 @@
 
 module pee.dao {
     requires java.sql;
+    requires org.mybatis;
     requires pee.model;
     
+    exports com.github.idelstak.pee.records.dao.api;
     exports com.github.idelstak.pee.records.dao.spi;
     exports com.github.idelstak.pee.records.dao.spi.impl;
 }

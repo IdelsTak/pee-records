1fe783955e9e821a824f1ea626aad8412a3c056f
Hiram K <hiram.kamau@outlook.com>
2021-08-16 17:27:58 +0300

    Implement database settings get/set and connection test

--- pee-records-controller/src/main/java/com/github/idelstak/pee/records/controller/database/DatabaseConnectionController.java
+++ pee-records-controller/src/main/java/com/github/idelstak/pee/records/controller/database/DatabaseConnectionController.java
@@ -3,11 +3,24 @@
  */
 package com.github.idelstak.pee.records.controller.database;
 
+import com.github.idelstak.pee.records.controller.ConnectionPreferences;
+import com.github.idelstak.pee.records.database.DatabaseAccess;
+import java.net.URI;
+import java.sql.Connection;
+import java.util.Optional;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -16,9 +29,9 @@ import javafx.scene.control.TextField;
 public class DatabaseConnectionController {
 
     @FXML
-    private TextField hostTextField;
+    private DialogPane connectionDialogPane;
     @FXML
-    private TextField portTextField;
+    private TextField hostTextField;
     @FXML
     private TextField databaseNameTextField;
     @FXML
@@ -31,9 +44,102 @@ public class DatabaseConnectionController {
     private Button testConnectionButton;
     @FXML
     private Label connectionStatusLabel;
+    @FXML
+    private ProgressIndicator connectionProgressIndicator;
+    @FXML
+    private Label connectionFailureDetailsLabel;
+    private final ConnectionPreferences preferences;
+    private final ButtonType saveButtonType;
+
+    public DatabaseConnectionController(ConnectionPreferences preferences) {
+        this.preferences = preferences;
+        this.saveButtonType = new ButtonType("Save", ButtonBar.ButtonData.OK_DONE);
+    }
 
     @FXML
     void initialize() {
+        //Add the button types
+        connectionDialogPane.getButtonTypes().addAll(saveButtonType, ButtonType.CANCEL);
 
+        Button saveBtn = (Button) connectionDialogPane.lookupButton(saveButtonType);
+
+        saveBtn.addEventFilter(ActionEvent.ACTION, eh -> {
+            String user = usernameTextField.getText();
+            String password = passwordTextfield.getText();
+            char[] pass = password == null ? new char[]{} : password.toCharArray();
+            String URL = urlTextField.getText();
+
+            preferences.setUrl(URL);
+            preferences.setUsername(user);
+            preferences.setPassword(pass);
+        });
+
+        String url = preferences.getUrl();
+        if (url != null && url.length() > 5) {
+            URI uri = URI.create(url.substring(5));
+
+            hostTextField.setText(uri.getHost());
+            databaseNameTextField.setText(uri.getPath().substring(1));
+
+            urlTextField.setText(url);
+        }
+
+        usernameTextField.setText(preferences.getUsername());
+        passwordTextfield.setText(new String(preferences.getPassword()));
+
+        hostTextField.textProperty().addListener((o, ov, nv) -> {
+            updateUrl(nv, databaseNameTextField.getText());
+        });
+
+        databaseNameTextField.textProperty().addListener((o, ov, nv) -> {
+            updateUrl(hostTextField.getText(), nv);
+        });
+
+        testConnectionButton.setOnAction(eh -> doTestConnection());
     }
+
+    public ButtonType getSaveButton() {
+        return saveButtonType;
+    }
+
+    private void doTestConnection() {
+        String user = usernameTextField.getText();
+        String password = passwordTextfield.getText();
+        char[] pass = password == null ? new char[]{} : password.toCharArray();
+        String URL = urlTextField.getText();
+        DatabaseAccess access = new DatabaseAccess(user, pass, URL);
+
+        Service<Optional<Connection>> connectService = new Service<Optional<Connection>>() {
+            @Override
+            protected Task<Optional<Connection>> createTask() {
+                return new DatabaseConnectionTask(access);
+            }
+        };
+
+        connectionStatusLabel.textProperty().bind(connectService.titleProperty());
+        connectionFailureDetailsLabel.textProperty().bind(connectService.messageProperty());
+        connectionProgressIndicator.visibleProperty().bind(connectService.runningProperty());
+        connectionProgressIndicator.progressProperty().bind(connectService.progressProperty());
+
+        connectService.setOnSucceeded(wse -> {
+            Optional<Connection> optionalConn = (Optional<Connection>) wse.getSource().getValue();
+
+            optionalConn.ifPresentOrElse(
+                    conn -> {
+                        connectionStatusLabel.setTextFill(Color.GREEN);
+                        access.closeConnection();
+                    },
+                    () -> connectionStatusLabel.setTextFill(Color.RED)
+            );
+        });
+
+        connectService.setOnRunning(eh -> connectionStatusLabel.setTextFill(Color.BLACK));
+
+        connectService.start();
+    }
+
+    private void updateUrl(String host, String name) {
+        urlTextField.setText("jdbc:mysql://%s/%s".formatted(host, name));
+    }
+
 }

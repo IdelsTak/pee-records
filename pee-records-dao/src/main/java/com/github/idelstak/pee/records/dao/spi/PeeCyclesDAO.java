2520b4f18bf552692f52fcab05d5b1232939403d
Hiram K <hiram.kamau@outlook.com>
2021-08-17 07:39:25 +0300

    Reflect the renaming of PeeRecords as PeeCycles

--- pee-records-dao/src/main/java/com/github/idelstak/pee/records/dao/spi/PeeCyclesDAO.java
+++ pee-records-dao/src/main/java/com/github/idelstak/pee/records/dao/spi/PeeCyclesDAO.java
@@ -3,23 +3,23 @@
  */
 package com.github.idelstak.pee.records.dao.spi;
 
-import com.github.idelstak.pee.records.model.spi.PeeRecord;
 import com.github.idelstak.pee.records.model.spi.core.Entity;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Optional;
+import com.github.idelstak.pee.records.model.spi.PeeCycle;
 
 /**
  *
  * @author Hiram K. <https://github.com/IdelsTak>
  */
-public interface PeeRecordsDAO {
+public interface PeeCyclesDAO {
 
-    Iterable<PeeRecord> getAllPeeRecords() throws IOException;
+    Iterable<PeeCycle> getAllPeeRecords() throws IOException;
 
-    Iterable<PeeRecord> getPatientPeeRecords(Entity patientId) throws IOException;
+    Iterable<PeeCycle> getPatientPeeRecords(Entity patientId) throws IOException;
 
-    Optional<PeeRecord> getPeeRecord(Entity peeRecordId) throws IOException;
+    Optional<PeeCycle> getPeeRecord(Entity peeRecordId) throws IOException;
 
     Entity addPeeRecord(Entity patientId, LocalDate startDate) throws IOException;
 

54ece3b01dfe216421c31a2e6861f67668fddc5c
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-22 12:06:47 +0300

    Update MySqlPeeCyclesDao.java

--- pee-records-dao/src/main/java/com/github/idelstak/pee/records/dao/impl/MySqlPeeCyclesDao.java
+++ pee-records-dao/src/main/java/com/github/idelstak/pee/records/dao/impl/MySqlPeeCyclesDao.java
@@ -41,7 +41,7 @@ public class MySqlPeeCyclesDao implements PeeCyclesDao {
 
         String sql = "SELECT id FROM pee_cycles";
 
-        try (Connection conn = dataSource.getConnection(); Statement stmt = conn.createStatement(); ResultSet rset = stmt.executeQuery(sql)) {
+        try ( Connection conn = dataSource.getConnection();  Statement stmt = conn.createStatement();  ResultSet rset = stmt.executeQuery(sql)) {
             while (rset.next()) {
                 allCycles.add(new MySqlPeeCycle(dataSource, rset.getInt(1)));
             }
@@ -81,14 +81,14 @@ public class MySqlPeeCyclesDao implements PeeCyclesDao {
 
         String sql = "INSERT INTO pee_cycles (patient_id, start_date, end_date) VALUES (?, ?, ?)";
 
-        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+        try ( Connection conn = dataSource.getConnection();  PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             stmt.setInt(1, patientId.getId());
             stmt.setDate(2, Date.valueOf(startDate));
             stmt.setDate(3, Date.valueOf(startDate.plusWeeks(13L)));
 
             stmt.executeUpdate();
 
-            try (ResultSet rset = stmt.getGeneratedKeys()) {
+            try ( ResultSet rset = stmt.getGeneratedKeys()) {
                 if (rset.next()) {
                     int cycleId = rset.getInt(1);
 
@@ -112,7 +112,7 @@ public class MySqlPeeCyclesDao implements PeeCyclesDao {
 
         String sql = "UPDATE pee_cycles SET start_date = ?, end_date = ? WHERE id = ?";
 
-        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try ( Connection conn = dataSource.getConnection();  PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setDate(1, Date.valueOf(newStartDate));
             stmt.setDate(2, Date.valueOf(newStartDate.plusWeeks(13L)));
             stmt.setInt(3, cycleId.getId());
@@ -133,7 +133,7 @@ public class MySqlPeeCyclesDao implements PeeCyclesDao {
 
         String sql = "UPDATE pee_cycles SET start_date = ?, end_date = ? WHERE id = ?";
 
-        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try ( Connection conn = dataSource.getConnection();  PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setDate(1, Date.valueOf(newEndDate.minusWeeks(13L)));
             stmt.setDate(2, Date.valueOf(newEndDate));
             stmt.setInt(3, cycleId.getId());
@@ -152,9 +152,9 @@ public class MySqlPeeCyclesDao implements PeeCyclesDao {
 
         String sql = "DELETE FROM pee_cycles WHERE id = ?";
 
-        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try ( Connection conn = dataSource.getConnection();  PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, cycleId.getId());
-            
+
             stmt.executeUpdate();
         } catch (SQLException ex) {
             throw new IOException(ex);
